3f1c53f868942603e0a148586fa1c852f9bdac1e
Cameron Saidock <saidock@usc.edu>
2022-12-14 16:01:22 -0800

    First commit

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './About.css'
+
+function About() {
+  return (
+    <div className='About-Holder'>
+
+    </div>
+  )
+}
+
+export default About
---
+++ src/Components/Intro/Intro.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Intro.css'
+
+function Intro() {
+  return (
+    <div className='Intro-Holder'>
+        <div className='Intro'>
+            <div className='Intro-Text'>
+                <div className='Name'>
+                    <h1>Cameron Saidock</h1>
+                </div>
+                <div className='Roles'>
+                    <h2>Entrepreneur • Software Engineer • Consultant</h2>
+                </div>
+            </div>
+            <div className='Intro-Fade'/>
+        </div>
+    </div>
+  )
+}
+
+export default Intro
